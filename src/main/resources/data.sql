df49b788255e7c5edf535f3dcfc683bb6cf992e1
sunyesle <sunyesle@gmail.com>
2024-08-08 00:14:32 +0900

    chore: 초기 데이터 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into member (member_id, email, name, password, phone_number, role) values
+(default, 'user1@email.com', '사용자1', '$2a$10$SasEKIJ2qQyww3Gsq32qpuMhVGE9.NuUqQlShKH8infazDtEfV1OO', '010-1111-1111', 'USER');
