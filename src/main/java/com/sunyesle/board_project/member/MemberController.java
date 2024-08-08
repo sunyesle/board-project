4f7f2b9c542907c8c319c984713911beae9824a6
sunyesle <sunyesle@gmail.com>
2024-08-08 21:39:32 +0900

    feat: 회원가입 요청 유효성 검증 적용

--- src/main/java/com/sunyesle/board_project/member/MemberController.java
+++ src/main/java/com/sunyesle/board_project/member/MemberController.java
@@ -1,6 +1,7 @@
 package com.sunyesle.board_project.member;
 
 import com.sunyesle.board_project.common.dto.CreateResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<CreateResponse> signup(@RequestBody MemberRequest request){
+    public ResponseEntity<CreateResponse> signup(@Valid @RequestBody MemberRequest request){
         CreateResponse response = memberService.signup(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
