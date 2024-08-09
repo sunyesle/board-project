721c15ee086f21b82c811fe8f3d3b5711723f104
sunyesle <sunyesle@gmail.com>
2024-08-10 02:14:32 +0900

    feat: 게시글 작성 유효성 검증 추가

--- src/main/java/com/sunyesle/board_project/board/BoardRequest.java
+++ src/main/java/com/sunyesle/board_project/board/BoardRequest.java
@@ -1,10 +1,18 @@
 package com.sunyesle.board_project.board;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class BoardRequest {
+
+    @NotBlank(message = "제목은 필수 입력 항목입니다.")
+    @Size(max = 200, message = "제목은 200자 이하여야 합니다")
     private final String title;
+
+    @NotBlank(message = "내용은 필수 입력 항목입니다.")
+    @Size(max = 1000, message = "내용은 1000자 이하여야 합니다")
     private final String content;
 
     public BoardRequest(String title, String content) {
