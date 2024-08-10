1d0478c971a97c3a475717a1795d42d6962d4da7
sunyesle <sunyesle@gmail.com>
2024-08-10 19:00:12 +0900

    feat: 삭제된 게시글 조회되지 않도록 쿼리 수정

--- src/main/java/com/sunyesle/board_project/board/BoardRepository.java
+++ src/main/java/com/sunyesle/board_project/board/BoardRepository.java
@@ -11,10 +11,12 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query(value = "select new com.sunyesle.board_project.board.BoardResponse(b.id, b.title, b.content, b.createdAt, m.id, m.name) " +
             "from Board b join Member m on m.id = b.memberId " +
-            "where (:title is null or b.title like concat('%', :title, '%'))",
+            "where (:title is null or b.title like concat('%', :title, '%'))" +
+            "and b.deletedAt is null",
             countQuery = "select count(*) " +
                     "from Board b " +
-                    "where (:title is null or b.title like concat('%', :title, '%'))"
+                    "where (:title is null or b.title like concat('%', :title, '%'))" +
+                    "and b.deletedAt is null"
     )
     Page<BoardResponse> findByTitleContains(String title, Pageable pageable);
 
