05c5ffd4de21596273153bd48ea1b2e9e3d80e1a
sunyesle <sunyesle@gmail.com>
2024-12-02 20:21:56 +0900

    docs: tag 및 설명 추가

--- src/test/java/com/sunyesle/board_project/docs/BoardDocsTest.java
+++ src/test/java/com/sunyesle/board_project/docs/BoardDocsTest.java
@@ -1,5 +1,6 @@
 package com.sunyesle.board_project.docs;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.sunyesle.board_project.board.BoardController;
 import com.sunyesle.board_project.board.BoardService;
 import com.sunyesle.board_project.board.dto.BoardDetailResponse;
@@ -18,17 +19,19 @@ import org.springframework.http.MediaType;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.BDDMockito.given;
-import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BoardController.class)
 class BoardDocsTest extends BaseRestDocsTest {
+    private static final String TAG = "Board API";
 
     @MockBean
     private BoardService boardService;
@@ -47,14 +50,17 @@ class BoardDocsTest extends BaseRestDocsTest {
                         .content(objectMapper.writeValueAsString(boardRequest)))
                 .andExpect(status().isCreated())
                 .andDo(document("save-board",
-                        requestFields(
-                                fieldWithPath("title").description("제목"),
-                                fieldWithPath("content").description("내용")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("게시글 id")
-                        )
-                ));
+                        resource(ResourceSnippetParameters.builder()
+                                .tag(TAG)
+                                .description("게시글 작성")
+                                .requestFields(
+                                        fieldWithPath("title").description("제목"),
+                                        fieldWithPath("content").description("내용")
+                                )
+                                .responseFields(
+                                        fieldWithPath("id").description("게시글 id")
+                                )
+                                .build())));
     }
 
     @Test
@@ -67,20 +73,23 @@ class BoardDocsTest extends BaseRestDocsTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-board",
-                        pathParameters(
-                                parameterWithName("id").description("게시글 id")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("게시글 id"),
-                                fieldWithPath("title").description("게시글 제목"),
-                                fieldWithPath("content").description("게시글 내용"),
-                                fieldWithPath("createdAt").description("게시글 생성일시"),
-                                fieldWithPath("modificationDeadline").description("게시글 수정가능일시"),
-                                fieldWithPath("writer").description("작성자 정보"),
-                                fieldWithPath("writer.id").description("작성자 id"),
-                                fieldWithPath("writer.name").description("작성자 이름")
-                        )
-                ));
+                        resource(ResourceSnippetParameters.builder()
+                                .tag(TAG)
+                                .description("게시글 상세 조회")
+                                .pathParameters(
+                                        parameterWithName("id").description("게시글 id")
+                                )
+                                .responseFields(
+                                        fieldWithPath("id").description("게시글 id"),
+                                        fieldWithPath("title").description("게시글 제목"),
+                                        fieldWithPath("content").description("게시글 내용"),
+                                        fieldWithPath("createdAt").description("게시글 생성일시"),
+                                        fieldWithPath("modificationDeadline").description("게시글 수정가능일시"),
+                                        fieldWithPath("writer").description("작성자 정보"),
+                                        fieldWithPath("writer.id").description("작성자 id"),
+                                        fieldWithPath("writer.name").description("작성자 이름")
+                                )
+                                .build())));
     }
 
     @Test
@@ -101,28 +110,31 @@ class BoardDocsTest extends BaseRestDocsTest {
                         .param("orderBy", "LATEST"))
                 .andExpect(status().isOk())
                 .andDo(document("get-boards",
-                        queryParameters(
-                                parameterWithName("title").description("제목 검색 키워드").optional(),
-                                parameterWithName("pageNumber").description("페이지").optional(),
-                                parameterWithName("pageSize").description("페이지 사이즈").optional(),
-                                parameterWithName("orderBy").description("정렬").optional()
-                        ),
-                        responseFields(
-                                fieldWithPath("content").description("게시글 리스트"),
-                                fieldWithPath("content[].id").description("게시글 id"),
-                                fieldWithPath("content[].title").description("게시글 제목"),
-                                fieldWithPath("content[].content").description("게시글 내용"),
-                                fieldWithPath("content[].createdAt").description("게시글 작성일시"),
-                                fieldWithPath("content[].writer").description("작성자 정보"),
-                                fieldWithPath("content[].writer.id").description("작성자 id"),
-                                fieldWithPath("content[].writer.name").description("작성자 이름"),
-                                fieldWithPath("page").description("페이지 정보"),
-                                fieldWithPath("page.size").description("페이지 사이즈"),
-                                fieldWithPath("page.number").description("현재 페이지 번호"),
-                                fieldWithPath("page.totalElements").description("전체 요소 수"),
-                                fieldWithPath("page.totalPages").description("전체 페이지 수")
-                        )
-                ));
+                        resource(ResourceSnippetParameters.builder()
+                                .tag(TAG)
+                                .description("게시글 목록 조회")
+                                .queryParameters(
+                                        parameterWithName("title").description("제목 검색 키워드").optional(),
+                                        parameterWithName("pageNumber").description("페이지").optional(),
+                                        parameterWithName("pageSize").description("페이지 사이즈").optional(),
+                                        parameterWithName("orderBy").description("정렬").optional()
+                                )
+                                .responseFields(
+                                        fieldWithPath("content").description("게시글 리스트"),
+                                        fieldWithPath("content[].id").description("게시글 id"),
+                                        fieldWithPath("content[].title").description("게시글 제목"),
+                                        fieldWithPath("content[].content").description("게시글 내용"),
+                                        fieldWithPath("content[].createdAt").description("게시글 작성일시"),
+                                        fieldWithPath("content[].writer").description("작성자 정보"),
+                                        fieldWithPath("content[].writer.id").description("작성자 id"),
+                                        fieldWithPath("content[].writer.name").description("작성자 이름"),
+                                        fieldWithPath("page").description("페이지 정보"),
+                                        fieldWithPath("page.size").description("페이지 사이즈"),
+                                        fieldWithPath("page.number").description("현재 페이지 번호"),
+                                        fieldWithPath("page.totalElements").description("전체 요소 수"),
+                                        fieldWithPath("page.totalPages").description("전체 페이지 수")
+                                )
+                                .build())));
     }
 
     @Test
@@ -136,14 +148,17 @@ class BoardDocsTest extends BaseRestDocsTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isNoContent())
                 .andDo(document("update-board",
-                        pathParameters(
-                                parameterWithName("id").description("게시글 id")
-                        ),
-                        requestFields(
-                                fieldWithPath("title").description("제목"),
-                                fieldWithPath("content").description("내용")
-                        )
-                ));
+                        resource(ResourceSnippetParameters.builder()
+                                .tag(TAG)
+                                .description("게시글 수정")
+                                .pathParameters(
+                                        parameterWithName("id").description("게시글 id")
+                                )
+                                .requestFields(
+                                        fieldWithPath("title").description("제목"),
+                                        fieldWithPath("content").description("내용")
+                                )
+                                .build())));
     }
 
     @Test
@@ -153,9 +168,12 @@ class BoardDocsTest extends BaseRestDocsTest {
                         .header("Authorization", "Bearer {ACCESS_TOKEN}"))
                 .andExpect(status().isNoContent())
                 .andDo(document("delete-board",
-                        pathParameters(
-                                parameterWithName("id").description("게시글 id")
-                        )
-                ));
+                        resource(ResourceSnippetParameters.builder()
+                                .tag(TAG)
+                                .description("게시글 삭제")
+                                .pathParameters(
+                                        parameterWithName("id").description("게시글 id")
+                                )
+                                .build())));
     }
 }
